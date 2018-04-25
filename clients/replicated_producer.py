d938bb77d7ef640b433c96258813ee1de547582c
ACONCEPT <joesadaka@gmail.com>
2018-04-25 13:35:01 -0500

    first commit, get data

--- clients/replicated_producer.py
+++
@@ -1,8 +0,0 @@
-#! usr/bin/env python3
-import sys
-sys.path.append("../base/")
-from replic_producer import producer
-from producer_menu import prompt_message, producer_menu
-
-if __name__ == '__main__':
-    producer_menu(producer,topic = "replicated_topic")
