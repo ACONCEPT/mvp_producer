ebb5e1361e920b06530efc56603ec86f0f7c3dd9
ACONCEPT <joesadaka@gmail.com>
2018-04-26 18:58:54 -0500

    removed unnecessary directory

--- base/client.py
+++
@@ -1,4 +0,0 @@
-#! usr/bin/env python3
-
-from kafka import KafkaClient
-client = KafkaClient("localhost:9092")
--- base/replic_producer.py
+++
@@ -1,4 +0,0 @@
-#! usr/bin/env python3
-from kafka import KafkaProducer
-import sys
-producer = KafkaProducer(bootstrap_servers=['localhost:9092','localhost:9093','localhost:9094'])
